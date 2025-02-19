4ad508133713d3ddc7f72c9b2c3563f48a7a1861
Afdhal Fandu Wijaya <Afdhal20022000@gmail.com>
2025-02-19 17:20:49 +0700

    belajar hook useState dan useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,54 @@
 
+import { useState } from 'react';
 import './App.css'
-import { Button } from './component/button';
-import { Alert } from "./component/Navbar";
+import { useEffect } from 'react';
+
+// import { Button } from './component/button';
+// import { Navbar } from './component/Navbar';
 
 
 
 
 function App() {
+  // fieldset
+  const [name, setName] = useState("");
+  //  1.empty depedency(jln ketika pertamakali di render)
+  useEffect(() => {
+    console.log(name, "ini dari dalam use effect");
+  }, []);
+
+  //  2.with depedency(jln ketika pertamakali di render)
+  // useEffect(() => {
+  //   console.log("ini dari dalam use effect with depedency");
+  // }, [name]);
+
+  // useEffect(() => {
+  //   console.log("jalan setiap kali apapun state berubah ");
+  // });
+
   return (
     <>
-      <div className='flex place-content-between '>
-        <Alert></Alert>
-        <Button variant="primary">Register</Button>
-        <Button variant="secondery">Login</Button>
+      <div className='grid place-content-center h-screen'>
+        {name == "afdhal" ? <p>Hallo Boss</p> : <p>My name is: {name} </p>}
+
+        <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+          <legend className="fieldset-legend">Settings</legend>
+          <div className="join">
+            <input
+              type="text"
+              className="input join-item"
+              placeholder="Product name"
+
+              onChange={(e) => {
+                setName(e.target.value);
+              }} />
+
+            <button className="btn join-item">save</button>
+          </div>
+        </fieldset>
       </div>
+
+
     </>
 
   );
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,18 +1,39 @@
-export function Alert({ variant, children }) {
+export function Navbar({ variant, children }) {
     return (
-        <div role="alert" className="alert alert-info">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="h-6 w-6 shrink-0 stroke-current">
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-            </svg>
-            <span>New software update available.</span>
+        <div className="navbar bg-amber-500">
+            <div className="flex-none">
+                <button className="btn btn-square btn-ghost">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block h-5 w-5 stroke-current">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </button>
+            </div>
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="flex-none">
+                <button className="btn btn-square btn-ghost">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block h-5 w-5 stroke-current">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+                    </svg>
+                </button>
+            </div>
         </div>
     )
 
